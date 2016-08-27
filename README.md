7e0ef0153979a78c2bbb29ee9a5c1b97aa24297b
lisirrx <lisirrx@gmail.com>
2016-08-27 22:24:26 +0800

    no message

--- README.md
+++
@@ -1,1 +0,0 @@
-This is the registration website of SSESU, Tongji Unversity.
--- app/PDF/pdf.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-from .. import db
-from .. import Student
-
-
-
